2ac3aa8983d3b7bda23f60bc3928ab61f7d12704
yugimumoto1 <sonnytoe96@gmail.com>
2020-05-22 16:22:22 -0600

    finsihed

--- project/app/src/main/java/com/doria/david/intentsproject/DisplayScripture.java
+++ project/app/src/main/java/com/doria/david/intentsproject/DisplayScripture.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DisplayScripture extends AppCompatActivity {
@@ -14,6 +15,8 @@ public class DisplayScripture extends AppCompatActivity {
         setContentView(R.layout.activity_display_scripture);
         Intent intent = getIntent();
         String scripture = intent.getExtras().getString("scripture");
+        String logMsg = "Received Intent with: " + scripture;
+        Log.d("intentReceived", logMsg);
         TextView textView5 = (TextView) findViewById(R.id.textView5);
         textView5.setText(scripture);
     }
--- project/app/src/main/java/com/doria/david/intentsproject/MainActivity.java
+++ project/app/src/main/java/com/doria/david/intentsproject/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -17,15 +18,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view){
-        Intent displayScripture = new Intent(this, DisplayScripture.class);
+
+
 
         EditText getBook    = (EditText) findViewById(R.id.scripture_book);
         EditText getChapter = (EditText) findViewById(R.id.scripture_chapter);
         EditText getVerse   = (EditText) findViewById(R.id.scripture_verse);
 
         String scripture = getBook.getText().toString()    + " " +
-                           getChapter.getText().toString() + ":" +
-                           getVerse.getText().toString();
+                getChapter.getText().toString() + ":" +
+                getVerse.getText().toString();
+        String logMsg = "About to create Intent with: " + scripture;
+
+        Log.d("intentCreation", logMsg);
+
+        Intent displayScripture = new Intent(this, DisplayScripture.class);
+
+
 
         displayScripture.putExtra("scripture", scripture);
 
