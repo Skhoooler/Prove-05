0cfa40dc9485cf8e98d296650bde8cede8de0765
Skhoooler <dor15004@byui.edu>
2020-05-22 11:46:38 -0600

    Added base project files

---
+++ project/app/src/main/java/com/doria/david/intentsproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.doria.david.intentsproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
