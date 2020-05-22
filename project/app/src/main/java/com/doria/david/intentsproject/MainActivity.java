0f413e9ab642f48c3c0890ebacebe5daeab9a1b2
Skhoooler <dor15004@byui.edu>
2020-05-22 12:51:58 -0600

    fixed autofill issue

--- project/app/src/main/java/com/doria/david/intentsproject/MainActivity.java
+++ project/app/src/main/java/com/doria/david/intentsproject/MainActivity.java
@@ -2,11 +2,14 @@ package com.doria.david.intentsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    //public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view){
+        Intent displayScripture = new Intent(this, DisplayScripture.class);
 
+        EditText getBook    = (EditText) findViewById(R.id.scripture_book);
+        EditText getChapter = (EditText) findViewById(R.id.scripture_chapter);
+        EditText getVerse   = (EditText) findViewById(R.id.scripture_verse);
+
+        String scripture = getBook.getText().toString()    + " " +
+                           getChapter.getText().toString() + ":" +
+                           getVerse.getText().toString();
+
+        startActivity(displayScripture);
     }
 }
